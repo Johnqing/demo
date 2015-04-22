64cd3885f693c0560ea782641130e0a7e4a9151f
johnqing <csssnow@gmail.com>
2015-04-22 17:18:04 +0800

    first commit

---
+++ js/demo.js
@@ -0,0 +1,91 @@
+(function(){
+
+    var globel = {
+        number: 0
+    };
+
+    $('#choose').sortable({
+        ghostClass: 'ghost',
+        name: 'q',
+        group: {
+            pull: 'clone',
+            put: false
+        },
+        sort: false,
+        animation: 100,
+        onEnd: function(evt){
+            var node = $(evt.item);
+            if(node.parents('#choose').length) return;
+            node.html(NT.tpl('checkboxTmp', globel));
+            globel.number++;
+        }
+    });
+
+    $('#sortList').sortable({
+        name: 'q',
+        group: {
+            pull: false,
+            put: true
+        },
+        animation: 100
+    });
+
+
+    // 编辑
+
+    var editorConfig = {};
+    editorConfig.language = 'en';
+    editorConfig.resize_enabled = true;
+    editorConfig.removePlugins = 'elementspath';
+    editorConfig.disableNativeSpellChecker = false;
+    editorConfig.allowedContent = true;
+    editorConfig.title = false
+    editorConfig.extraPlugins = 'pbckcode';
+    editorConfig.toolbarGroups = [
+        {
+            name: 'basicstyles',
+            items: ['Bold', 'Italic', 'Underline']
+        },
+        {
+            name: 'paragraph',
+            items: ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'BidiLtr', 'Outdent', 'Indent', 'BidiRtl', 'bidi', 'Link']
+        },
+        {
+            name: 'insert',
+            items: ['Image', 'Table', 'Iframe', 'MediaEmbed']
+        },
+        {
+            name: "links"
+        },
+        {
+            name: "insert"
+        }, ['pbckcode']];
+
+    editorConfig.removeButtons = 'Subscript,Superscript,Anchor,strike,magicline';
+    editorConfig.extraPlugins += (editorConfig.extraPlugins.length == 0 ? '' : ',') + 'ckeditor_wiris';
+    editorConfig.toolbar = editorConfig.toolbarGroups;
+
+
+    function editor(){
+        layer.open({
+            type: 1,
+            area: ['630px', '585px'],
+            title: 0,
+            closeBtn: 0,
+            shadeClose: 0,
+            skin: 'm-q-dialog',
+            content: NT.tpl('dialogTmp', {})
+        });
+        CKEDITOR.disableAutoInline = true;
+        CKEDITOR.inline( 'editor01', editorConfig);
+    }
+
+    $('#sortList').on('click', 'li', function(){
+        editor();
+    });
+
+    $('body').on('click', '.closeDialog', function(){
+        layer.closeAll();
+    });
+
+})();
